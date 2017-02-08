4130a2b8ee152a0f032aca51248d1ae3cce84cc9
luckyadam <weitaozsh@gmail.com>
2017-02-08 14:22:58 +0800

    update:排序算法

---
+++ src/sort/bubble.js
@@ -0,0 +1,13 @@
+function bubbleSort (arr) {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
---
+++ src/sort/insertion.js
@@ -0,0 +1,15 @@
+function insertionSort (arr) {
+  const len = arr.length;
+  let preIndex = 0;
+  let current;
+  for (let i = 1; i < len; i++) {
+    preIndex = i - 1;
+    current = arr[i];
+    while (preIndex >= 0 && arr[preIndex] > current) {
+      arr[preIndex + 1] = arr[preIndex];
+      preIndex--;
+    }
+    arr[preIndex + 1] = current;
+  }
+  return arr;
+}
